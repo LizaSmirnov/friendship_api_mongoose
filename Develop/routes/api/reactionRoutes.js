f87522e30b8d467a4062d7650e08c16c80faa1e0
Liza Smirnov <krasnienkov@gmail.com>
2023-06-17 23:07:20 -0700

    cleaning

--- Develop/routes/api/reactionRoutes.js
+++ Develop/routes/api/reactionRoutes.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
-
 const {
-    
+  getReaction,
+  getSingleReaction,
+  createReaction,
+  updateReaction,
+  deleteReaction,
+} = require('../../controllers/courseController.js');
 
-} = require('../../controllers/reactionController')
+// /api/reactions
+router.route('/').get(getReaction).post(createReaction);
 
+// /api/reaction/:reactionId
+router
+  .route('/:courseId')
+  .get(getSingleReaction)
+  .put(updateReaction)
+  .delete(deleteReaction);
+
+module.exports = router;
